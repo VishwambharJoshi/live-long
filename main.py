164141e6977652db99eb2fc901cd97b15d1ff211
VishwambharJoshi <50924602+VishwambharJoshi@users.noreply.github.com>
2023-10-22 20:54:40 +0530

    Create main.py
    added code in main.py file

---
+++ main.py
@@ -0,0 +1,9 @@
+age = input("What is your current age?\n")
+
+
+years = 85 - int(age)
+months = round(years * 12)
+weeks = round(years * 52)
+days = round(years * 365)
+
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
